0804d49c2f9c2e7055e572ccd36a7e90a8e84732
Alejandro Vásquez Oviedo <alvaov@estudiantec.cr>
2021-03-22 22:39:55 -0600

    Creación de la bienvenida al usuario.

---
+++ WazeLog.pl
@@ -0,0 +1,9 @@
+%Análisis de lenguaje estructural
+
+%Grafo de lugares
+
+%Funcion Inicial
+iniciar():-
+    
+    write("Bienvenido a WazeLog la mejor logica de llegar a su destino. \n"),
+    write("Por Favor indique el lugar en el que se encuentra.").
