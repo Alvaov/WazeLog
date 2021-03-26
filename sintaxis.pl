6eb4a687761d0a57d7835a78cb4f3b0895581f8b
Luis Zuniga <luisandreyzuniga@gmail.com>
2021-03-26 16:07:43 -0600

    separando archivos

---
+++ sintaxis.pl
@@ -0,0 +1,83 @@
+:-consult(grafo).
+%Análisis de lenguaje estructural
+preposiciones->[];
+preposiciones-->[a].
+preposiciones-->[en].
+preposiciones-->[entre].
+preposiciones-->[hacia].
+preposiciones-->[hasta].
+preposiciones-->[mediante].
+preposiciones-->[para].
+preposiciones-->[por].
+preposiciones-->[segun].
+preposiciones-->[sin].
+preposiciones-->[so].
+preposiciones-->[sobre].
+preposiciones-->[tras].
+preposiciones-->[versus].
+
+articulo-->[].
+articulo-->[el].
+articulo-->[la].
+articulo-->[las].
+articulo-->[lo].
+articulo-->[los].
+articulo-->[un].
+articulo-->[unos].
+articulo-->[una].
+articulo-->[unas].
+
+
+verbo-->[viaje];[viajo];[viajar];[viajamos];[viajaremos].
+verbo-->[ir];[voy];[vaya];[va];[vamos];[vayan];[vengo];[van];[venir].
+verbo-->[pasar];[pase];[paso];[pasare];[pasaria];[pasase]. 
+verbo-->[llegar];[llego];[llegue];[llegare];[llegan].
+verbo-->[dirigir];[dirijo]; [dirigimos]; [dirigiste];[dirigire];[dirigiria].%
+verbo-->[trasladar];[traslado]; [trasladamos]; [trasladare]; [trasladaremos].%
+verbo-->[mover];[muevo];[movi];[movere];[moveremos].
+verbo-->[acudir];[acudi];[acudire];[acudo];[acudiremos];[acudiran].
+verbo-->[asistir];[asisto];[asistire];[asitiremos];[asistimos].
+verbo-->[recorrer];[recorro];[recorrere];[recorreremos];[recorremos].
+verbo-->[querer];[quiero];[queremos].
+
+
+pronombres-->[yo].
+pronombres-->[nosotros].
+pronombres-->[el].
+pronombres-->[ella].
+pronombres-->[ellas].
+pronombres-->[ellos].
+
+local-->[tienda].
+local-->[pulperia].
+local-->[supermercado].
+local-->[cine].
+local-->[farmacia].
+local-->[hospital].
+local-->[clinica].
+local-->[gimnasio].
+local-->[parque].
+local-->[municipalidad].
+local-->[banco].
+local-->[plaza].
+local-->[mall].
+local-->[centro].
+local-->[comercial].
+local-->[registro].
+local-->[carniceria].
+local-->[verduleria].
+local-->[puerto].
+local-->[piscina].
+local-->[ferreteria].
+
+
+
+oracion --> sintagma_nominal, sintagma_verbal.
+sintagma_nominal-->[].
+sintagma_nominal --> pronombres.
+sintagma_nominal --> articulo, local.
+sintagma_nominal --> {lugar(X)},[X].
+sintagma_verbal --> verbo, sintagma_nominal.
+
+
+lugar(X):-arco(X,_,_,_,_);arco(_,X,_,_,_).
